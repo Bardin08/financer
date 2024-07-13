0fd14ef789db922a7fa6aa4184374a461db41e8a
Bardin08 <vbardin810@gmail.com>
2024-07-13 13:23:23 +0300

    Define better API structure

---
+++ Financer.Api/DependencyInjection/LoggingDependencies.cs
@@ -0,0 +1,21 @@
+﻿using Serilog;
+using Serilog.Events;
+using X.Serilog.Sinks.Telegram.Extensions;
+
+namespace Financer.Api.DependencyInjection;
+
+public static class LoggingDependencies
+{
+    public static void AddLogging(this WebApplicationBuilder builder)
+    {
+        Log.Logger = new LoggerConfiguration()
+            .WriteTo.Console()
+            .WriteTo.TelegramCore(
+                builder.Configuration["Logging:Telegram:Token"]!,
+                builder.Configuration["Logging:Telegram:ChatId"]!,
+                LogEventLevel.Warning)
+            .CreateLogger();
+
+        builder.Logging.AddSerilog();
+    }
+}
---
+++ Financer.Api/Endpoints/HealthEndpoints.cs
@@ -0,0 +1,11 @@
+﻿namespace Financer.Api.Endpoints;
+
+public static class HealthEndpoints
+{
+    public static void MapHealthEndpoints(this IEndpointRouteBuilder endpoints)
+    {
+        var groupBuilder = endpoints.MapGroup("/api");
+
+        groupBuilder.MapGet("/healthz", () => Results.Ok("Healthy"));
+    }
+}
